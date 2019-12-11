5e1bfa8e38bd9d1b1115ebcf9b1029fc3313583e
PedroAKABlicas <44583830+PedroAKABlicas@users.noreply.github.com>
2019-12-11 16:45:35 +0000

    Delete Bolseiro.java

--- src/Bolseiro.java
+++
@@ -1,8 +0,0 @@
-public class Bolseiro extends Pessoa {
-    private Data datai, dataf;
-    public Bolseiro(String user, String email, Data diai, Data diaf) {
-        super(user, email);
-        this.datai = diai;
-        this.dataf = diaf;
-    }
-}
