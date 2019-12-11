3945728b0029d7f2b16377333089d17d38d79798
PedroAKABlicas <44583830+PedroAKABlicas@users.noreply.github.com>
2019-12-11 16:41:47 +0000

    Add files via upload

---
+++ untitled/src/Tarefa.java
@@ -0,0 +1,26 @@
+import java.io.Serializable;
+
+public class Tarefa implements Serializable{
+    private Data dataInicio, dataFim;
+    private int duaracao;
+    private Pessoa responsavel=null;
+    private String tipo;
+    private int taxa=0;
+
+    public Tarefa(Data datai, Data dataf, int duracao, String tipo){
+        this.dataInicio=datai;
+        this.dataFim=dataf;
+        this.duaracao=duracao;
+        this.tipo=tipo;
+    }
+
+    public void associaPessoa(Pessoa pessoa){
+        this.responsavel=pessoa;
+    }
+
+    public void mudaTaxa(int taxa){
+        this.taxa+=taxa;
+    }
+
+    public int getTaxa() {return taxa;}
+}
