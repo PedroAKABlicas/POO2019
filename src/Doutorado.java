156c146088d5e33928f4a4916bede167a93a46b6
PedroAKABlicas <44583830+PedroAKABlicas@users.noreply.github.com>
2019-12-11 16:46:03 +0000

    Delete Doutorado.java

--- src/Doutorado.java
+++
@@ -1,7 +0,0 @@
-public class Doutorado extends Bolseiro {
-    public Doutorado(String user, String email, Data diai, Data diaf) {
-        super(user, email, diai, diaf);
-    }
-
-    private String getTipo() {return "Doutorado";}
-}
